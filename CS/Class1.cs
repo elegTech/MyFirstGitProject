59e441ad5696c5e397a6ca088958ac4e5ff96e51
fans-PC\fans <fhrhx@126.com>
2017-09-06 14:53:00 +0800

    第一个加入Git的测试工程，Revit中用于任意区域铺贴的例子

---
+++ CS/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+
+namespace Revit.SDK.Samples.ParameterUtils.CS
+{
+    class Utility
+    {
+
+        public static List<Solid> GetSolid(Element element, Options option)
+        {
+            Contract.Requires(null != element && null != option);
+
+            GeometryElement geometryElement = element.get_Geometry(option);
+            List<Solid> solidList = new List<Solid>();
+            GeometryInstance geomInstance = null;
+
+            foreach (GeometryObject geoObj in geometryElement)
+            {
+                geomInstance = geoObj as GeometryInstance;
+                if (null == geomInstance)
+                    continue;
+
+                foreach (GeometryObject geom in geomInstance.SymbolGeometry)
+                {
+                    Solid solid = geom as Solid;
+                    if (null == solid || 0 == solid.Faces.Size || 0 == solid.Edges.Size)
+                    {
+                        continue;
+                    }
+                    solidList.Add(solid);
+                }
+            }
+
+            return solidList;
+        }
+
+        public static List<Face> GetFace(Solid solid)
+        {
+            Contract.Requires(null != solid);
+            List<Face> faceList = new List<Face>();
+
+            foreach (Face geomFace in solid.Faces)
+            {
+
+            }
+            return null;
+        }
+
+
+    }
+}
---
+++ CS/FamilyInstanceResizer.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace Revit.SDK.Samples.ParameterUtils.CS
+{
+    class FamilyInstanceResizer
+    {
+        private ElementId elementID;
+
+        private UIApplication app;
+
+        private Element element;
+        public FamilyInstanceResizer(UIApplication app, ElementId id)
+        {
+            Contract.Requires(null != app && null != id);
+            this.app = app;
+            this.elementID = id;
+
+            InitializeComponent();
+        }
+
+        public FamilyInstanceResizer(UIApplication app, Element element)
+        {
+            Contract.Requires(null != app && null != element);
+            this.app = app;
+            this.element = element;
+
+            InitializeComponent();
+        }
+
+        private bool InitializeComponent()
+        {
+            if (null != element)
+                return true;
+
+            element = app.ActiveUIDocument.Document.GetElement(elementID);
+            if (null == element)
+                return false;
+
+            return true;
+        }
+
+        private Options GetGeometryOption()
+        {
+            Options option = app.Application.Create.NewGeometryOptions();
+            if (null == option)
+                return null;
+
+            option.ComputeReferences = true;
+            option.DetailLevel = ViewDetailLevel.Fine;
+            
+            return option;
+        }
+
+        private List<Solid> GetSolid()
+        {
+            Options option = GetGeometryOption();
+            GeometryElement geometryElement = element.get_Geometry(option);
+
+            List<Solid> solidList = new List<Solid>();
+            GeometryInstance geomInstance = null;
+
+            foreach (GeometryObject geoObj in geometryElement)
+            {
+                geomInstance = geoObj as GeometryInstance;
+                if (null == geomInstance)
+                    continue;
+
+                foreach(GeometryObject geom in geomInstance.SymbolGeometry)
+                {
+                    Solid solid = geom as Solid;
+                    if (null == solid || 0 == solid.Faces.Size || 0 == solid.Edges.Size)
+                    {
+                        continue;
+                    }
+                    solidList.Add(solid);
+                }
+            }
+            
+            return solidList;
+        }
+
+        private List<Face> GetFace(Solid solid)
+        {
+            Contract.Requires(null != solid);
+            List<Face> faceList = new List<Face>();
+
+            foreach(Face geomFace in solid.Faces)
+            { }
+            return null;
+        }
+
+        
+        public bool ResizeElement()
+        {
+           
+            List<Solid> solidList = GetSolid();
+            if (solidList.Count == 0)
+                return false;
+
+
+            FaceArray faceArray = solidList[0].Faces;
+            List<double> areaList = new List<double>(faceArray.Size);
+
+            int top = 0;
+            int bottom = 0;
+            for (int i = 0; i < faceArray.Size; i++)
+            {
+                areaList.Add(faceArray.get_Item(i).Area);
+                if (i > 0)
+                {
+                    if (areaList[i] > areaList[top])
+                        top = i;
+                }
+            }
+            for (int i = 0; i < areaList.Count; i++)
+            {
+                if (i == top)
+                    continue;
+                if (Math.Abs(areaList[i]-areaList[top]) < 0.001)
+                {
+                    bottom = i;
+                }
+            }
+
+            Face topFace = faceArray.get_Item(top);
+            Face bottomFace = faceArray.get_Item(bottom);
+
+            FamilyInstance instance = element as FamilyInstance;
+            if (null != instance)
+            {
+                Transform transform = instance.GetTransform();
+                transform.ScaleBasis(2);
+            }
+
+
+
+
+            return true;
+        
+        }
+
+
+    }
+}
